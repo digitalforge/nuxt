f0005eb633ac54d5e324d9f6ab412e433ee7bf0e
Jonathan Doucette <jon@digitalforgedesign.com>
2024-03-29 21:23:57 -0400

    added nuxt products app

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi qui, quam
+      deleniti fuga hic dolorem, rerum eum quis maxime a illum totam facilis
+      amet dolore labore incidunt, cum ipsa. Aliquid.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia tempore
+      fuga facere perspiciatis sunt suscipit autem adipisci recusandae molestiae
+      dolores quisquam nobis officia molestias aliquam necessitatibus, tempora
+      maiores magnam quos.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+</style>
